8dbe6abd7a64eaae4c1a3665ed15b8c1f796fd4a
Viktor Kovalev <viktor.kovalev96@gmail.com>
2026-05-18 12:37:36 +0200

    staging weather hourly

--- models/staging/staging_weather_hourly.sql
+++ models/staging/staging_weather_hourly.sql
@@ -14,13 +14,13 @@ WITH hourly_raw AS (
         ,(json_data->>'dwpt')::NUMERIC AS dewpoint_c
         ,(json_data->>'rhum')::NUMERIC AS humidity_perc
         ,(json_data->>'prcp')::NUMERIC AS precipitation_mm
-        ,(json_data->>'snow')::INTEGER AS snow_mm
+        ,(json_data->>'snow')::NUMERIC)::INTEGER AS snow_mm
         ,((json_data->>'wdir')::NUMERIC)::INTEGER AS wind_direction
         ,(json_data->>'wspd')::NUMERIC AS wind_speed_kmh
         ,(json_data->>'wpgt')::NUMERIC AS wind_peakgust_kmh
         ,(json_data->>'pres')::NUMERIC AS pressure_hpa 
-        ,(json_data->>'tsun')::INTEGER AS sun_minutes
-        ,(json_data->>'coco')::INTEGER AS condition_code
+        ,(json_data->>'tsun')::NUMERIC)::INTEGER AS sun_minutes
+        ,(json_data->>'coco')::NUMERIC)::INTEGER AS condition_code
     FROM hourly_raw
     )
     SELECT * 
